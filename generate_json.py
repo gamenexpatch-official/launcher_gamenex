fc4470bf21c2cdb092155a8881d695d2144c5518
gamenexpatch-official <gamenexpatch@gmail.com>
2026-03-19 03:08:19 +0700

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -4,7 +4,7 @@ import json
 
 dbx = dropbox.Dropbox(os.environ["DROPBOX_TOKEN"])
 
-ROOT_FOLDER = "/Face Library 18"
+ROOT_FOLDER = "/FaceLibrary18"
 result = []
 
 def get_or_create_shared_link(path):
